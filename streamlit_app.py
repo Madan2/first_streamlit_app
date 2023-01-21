11c226a9621d579e5bedee49423d7dcefc46b63d
Madan <tnmadan@hotmail.com>
2023-01-21 20:33:56 +0000

    Added more items to the menu
    Added more items to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free Range Egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🍎🍇Omega 3 & Blueberry Oatmeal🍍🍞')
+streamlit.text('🥬🥣Kale, Spinach & Rocket Smoothie🥑🍊')
+streamlit.text('🐔🐔Hard-boiled Free Range Egg🐔🐔')
+streamlit.text('🥑🥑Avocado Toast🥑🥑')
+
+streamlit.header('🍍🍊Build your own fruit smoothie!🍍🍎')
