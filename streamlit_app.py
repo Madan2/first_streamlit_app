f553c69736daa05d9fa0fc031e70c22fb14f79d1
Madan <tnmadan@hotmail.com>
2023-01-22 16:42:00 +0000

    added Fruit Advice header
    added Fruit Advice header

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display the page on the list
 streamlit.dataframe(fruits_to_show)
 
+
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
