0d6443561b5782c48ef5616ac384ce554b5e1e13
Madan <tnmadan@hotmail.com>
2023-01-29 21:29:41 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,38 +22,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display the page on the list
 streamlit.dataframe(fruits_to_show)
-
-# create the repeatable code block (called function)
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
-    
-# New section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
-
-   
-
- # Allow the end user to add a fruit to the list
-streamlit.header("The fruit load list contains:")
-# snowflake related functions
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("select " from fruit_load_list")
-        return my_cur.fetchall()
- 
- # add a button to load the list
-if streamlit.button('Get fruit load list'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    streamlit.dataframe(my_data_rows)
     
     
     
