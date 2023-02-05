d6bee28a83051adc34b8a961db162cd458fff3fc
Madan <tnmadan@hotmail.com>
2023-02-05 20:18:06 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Favourites')
 streamlit.text('🍎🍇Omega 3 & Blueberry Oatmeal🍍🍞')
 streamlit.text('🥬🥣Kale, Spinach & Rocket Smoothie🥑🍊')
