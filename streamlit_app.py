69bc062a84dfa83e5c75a02de17313084556e6d5
Madan <tnmadan@hotmail.com>
2023-02-05 20:50:29 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ except URLError as e:
     streamlit.error()
 
 
-streamlit.header("View Our Fruit Load List - Add Your Favourites!")
+streamlit.header("View Our Fruit List - Add Your Favourites!")
 # snowflake related functions
 def get_fruit_load_list():
         with my_cnx.cursor() as my_cur:
